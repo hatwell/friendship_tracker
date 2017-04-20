1fb1fa90ed2984134c09539c9147241dc60a38c7
hatwell <caroline.hatwell@gmail.com>
2017-04-20 15:18:06 +0100

    set up project

---
+++ app/src/main/java/example/codeclan/com/friendshiptracker/Friend.java
@@ -0,0 +1,21 @@
+package example.codeclan.com.friendshiptracker;
+
+/**
+ * Created by user on 20/04/2017.
+ */
+
+public class Friend {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int totalDays;
+    private int String firstName;
+
+    public Friend(String firstName, String lastName, int totalDays) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.totalDays = totalDays;
+        this.
+    }
+
+}
---
+++ app/src/main/java/example/codeclan/com/friendshiptracker/MainActivity.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.friendshiptracker;
+
+/**
+ * Created by user on 20/04/2017.
+ */
+
+public class MainActivity {
+}
