5927bc46a2c354d8ddd90a01bccfbb98d01dd8ca
hatwell <caroline.hatwell@gmail.com>
2017-04-20 15:34:17 +0100

    created friend class and getter and setter methods

--- app/src/main/java/example/codeclan/com/friendshiptracker/Friend.java
+++ app/src/main/java/example/codeclan/com/friendshiptracker/Friend.java
@@ -1,5 +1,7 @@
 package example.codeclan.com.friendshiptracker;
 
+import java.util.Date;
+
 /**
  * Created by user on 20/04/2017.
  */
@@ -9,13 +11,43 @@ public class Friend {
     private String firstName;
     private String lastName;
     private int totalDays;
-    private int String firstName;
+    private int daysRemaining;
+    private Date dateSet;
 
     public Friend(String firstName, String lastName, int totalDays) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.totalDays = totalDays;
-        this.
+        this.daysRemaining = totalDays;
+        this.dateSet =
+
     }
 
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public int getTotalDays() {
+        return totalDays;
+    }
+
+    public int getDaysRemaining() {
+        return daysRemaining;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String LastName){
+        this.lastName = lastName;
+    }
+
+    public void setTotalDays(int totalDays) {
+        this.totalDays = totalDays;
+    }
 }
