3565f823a11259267bbf3962c14de309b32fb36b
hatwell <caroline.hatwell@gmail.com>
2017-04-27 09:41:57 +0100

    added welcome pasge

--- app/src/main/java/example/codeclan/com/friendshiptracker/HomeScreenActivity.java
+++ app/src/main/java/example/codeclan/com/friendshiptracker/HomeScreenActivity.java
@@ -1,13 +1,27 @@
 package example.codeclan.com.friendshiptracker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    private Button enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        enterButton = (Button) findViewById(R.id.enter_button);
+    }
+
+    public void enterButtonClicked(View button){
+
+        Intent intent = new Intent(this, AddFriendActivity.class);
+
+        startActivity(intent);
     }
 }
